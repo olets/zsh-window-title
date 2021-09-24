958e38be1b72ed9bb9ea1d259d79c9d06409acf6
Henry Bley-Vroman <olets@olets.dev>
2021-09-24 13:41:03 -1000

    style(functions): alphabetize

--- zsh-window-title.zsh
+++ zsh-window-title.zsh
@@ -3,6 +3,30 @@
 # A zsh plugin for informative terminal window titles
 # Copyright © 2021 Henry Bley-Vroman
 
+
+
+
+__zwt-add-hooks() {
+  'builtin' 'emulate' -LR zsh
+
+  # update window title before drawing the prompt
+  add-zsh-hook precmd __zwt-set-window-title-idle
+
+  # update the window title before executing a command
+  add-zsh-hook preexec __zwt-set-window-title-running
+}
+
+__zwt-init() {
+  'builtin' 'emulate' -LR zsh
+
+  __zwt-set-window-title-idle
+
+  # enable hooks
+  'builtin' 'autoload' -U add-zsh-hook
+  
+  __zwt-add-hooks
+}
+
 __zwt-set-window-title-idle() {
   # set the window title to
   # <parent dir>/<current dir>
@@ -23,27 +47,6 @@ __zwt-set-window-title-running() {
   local title=$(print -P "%2~ - ${1[(w)1]}")
 
   'builtin' 'echo' -ne "\033]0;$title\007"
-}
-
-__zwt-add-hooks() {
-  'builtin' 'emulate' -LR zsh
-
-  # update window title before drawing the prompt
-  add-zsh-hook precmd __zwt-set-window-title-idle
-
-  # update the window title before executing a command
-  add-zsh-hook preexec __zwt-set-window-title-running
-}
-
-__zwt-init() {
-  'builtin' 'emulate' -LR zsh
-  
-  __zwt-set-window-title-idle
-
-  # enable hooks
-  'builtin' 'autoload' -U add-zsh-hook
-  
-  __zwt-add-hooks
 }
 
 __zwt-init
